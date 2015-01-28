b5034f41ab1d18cfd85c8c362437927bca4e064c
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2015-01-28 15:03:52 -0600

    grunt default to jshint and mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,4 +14,6 @@ module.exports = function(grunt) {
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha');
+
+  grunt.registerTask('default', ['jshint', 'mocha']);
 };
