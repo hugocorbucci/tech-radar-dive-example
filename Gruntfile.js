5b9f9ca710b0a857be0266f8be008db5deda51e5
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2015-01-28 14:50:29 -0600

    Adding grunt-mocha although there are no tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,16 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       src: ['Gruntfile.js']
+    },
+    mocha: {
+      test: {
+        src: ['tests/**/*.html'],
+        options: {
+          run: true
+        }
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha');
 };
